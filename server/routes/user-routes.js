76d7e1b8ae75fdab484bfbdb28849c7629383da9
zachary <zacharyberdell@gmail.com>
2022-03-19 19:42:05 -0600

    Added all routes needed for users.

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -9,6 +9,7 @@ AWS.config.update(awsConfig);
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const table = 'Thoughts';
 
+//Get all users
 router.get('/users', (req, res) => {
     const params = {
       TableName: table,
@@ -18,6 +19,58 @@ router.get('/users', (req, res) => {
           res.status(500).json(err); // an error occurred
         } else {
           res.json(data.Items);
+        }
+    });
+});
+
+//Get one user
+router.get('/users/:username', (req, res) => {
+    console.log(`Querying for thought(s) from ${req.params.username}.`);
+
+    const params = {
+        TableName: table,
+        KeyConditionExpression: '#un = :user',
+        ExpressionAttributeNames: {
+          '#un': 'username',
+          '#ca': 'createdAt',
+          '#th': 'thought',
+        },
+        ExpressionAttributeValues: {
+          ':user': req.params.username,
+        },
+        ProjectionExpression: '#th, #ca',
+        ScanIndexForward: false,
+    };
+
+    dynamodb.query(params, (err, data) => {
+        if (err) {
+          console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+          res.status(500).json(err); // an error occurred
+        } else {
+          console.log("Query succeeded.");
+          res.json(data.Items)
+        }
+    });
+});
+
+//Add a user
+router.post('/users', (req, res) => {
+    const params = {
+      TableName: table,
+      Item: {
+        username: req.body.username,
+        createdAt: Date.now(),
+        thought: req.body.thought,
+      },
+    };
+    // database call
+    dynamodb.put(params, (err, data) => {
+        if (err) {
+          console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+          res.status(500).json(err); // an error occurred
+        } else {
+          console.log("Added item:", JSON.stringify(data, null, 2));
+          res.json({"Added": JSON.stringify(data, null, 2)});
         }
     });
 });
