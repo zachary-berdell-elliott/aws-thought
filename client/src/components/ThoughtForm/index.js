3fe0dfcf683bfb62a1a63fe09aea0b754e52222e
zachary <zacharyberdell@gmail.com>
2022-03-27 17:12:27 -0600

    Users can now upload images to the s3 bucket

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const ThoughtForm = () => {
   const [formState, setFormState] = useState({
@@ -6,6 +6,7 @@ const ThoughtForm = () => {
     thought: "",
   });
   const [characterCount, setCharacterCount] = useState(0);
+  const fileInput = useRef(null);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -14,6 +15,29 @@ const ThoughtForm = () => {
       setCharacterCount(event.target.value.length);
     }
   };
+  
+  const handleImageUpload = (event) => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append('image', fileInput.current.files[0]);
+    // send image file to endpoint with the postImage function
+    const postImage = async () => {
+      try {
+        const res = await fetch('/api/image-upload', {
+          mode: 'cors',
+          method: 'POST',
+          body: data,
+        });
+        if (!res.ok) throw new Error(res.statusText);
+        const postResponse = await res.json();
+        setFormState({ ...formState, image: postResponse.Location });
+        return postResponse.Location;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    postImage();
+  }
 
   // submit form
   const handleFormSubmit = (event) => {
@@ -64,8 +88,8 @@ const ThoughtForm = () => {
         ></textarea>
         <label className="form-input col-12  p-1">
           Add an image to your thought:
-        <input type="file" ref="{fileInput}" className="form-input p-2" />
-        <button className="btn" onClick="{handleImageUpload}" type="submit">
+        <input type="file" ref={fileInput} className="form-input p-2" />
+        <button className="btn" onClick={handleImageUpload} type="submit">
           Upload
         </button>
 </label>
