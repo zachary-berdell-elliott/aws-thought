f64b696e6e3ad2214f527539154ae9fac17d18fa
zachary <zacharyberdell@gmail.com>
2022-03-27 11:16:29 -0600

    Finished the route for uploading an image.

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -62,6 +62,13 @@ const ThoughtForm = () => {
           className="form-input col-12 "
           onChange={handleChange}
         ></textarea>
+        <label className="form-input col-12  p-1">
+          Add an image to your thought:
+        <input type="file" ref="{fileInput}" className="form-input p-2" />
+        <button className="btn" onClick="{handleImageUpload}" type="submit">
+          Upload
+        </button>
+</label>
         <button className="btn col-12 " type="submit">
           Submit
         </button>
--- server/routes/image-upload.js
+++ server/routes/image-upload.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const AWS = require('aws-sdk');
+const paramsConfig = require('../utils/params-config');
 
 const s3 = new AWS.S3({
     apiVersion: '2006-03-01',
@@ -17,6 +18,15 @@ const storage = multer.memoryStorage({
 const upload = multer({ storage }).single('image');
 
 router.post('/image-upload', upload, (req, res) => {
-    // set up params config
-    // set up S3 service call
-  });
+    const params = paramsConfig(req.file);
+
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      }
+      res.json(data);
+    });
+});
+
+module.exports = router;
